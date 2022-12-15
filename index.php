ca3ee7c42d7266aa0d63f29616ac3376c7fd84cc
llalex-esp <alejandro@alejandroalsa.es>
2022-12-15 23:45:15 +0100

    Fin version 1.0 de Send E-Mail PHP
    Signed-off-by: llalex-esp <alejandro@alejandroalsa.es>

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <!-- CSS only Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <!-- JavaScript Bundle with Popper Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <!-- Icons Bootstrap -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+  <title>Send E-Mail PHP | alejandroalsa</title>
+</head>
+<body>
+  <div class="container py-5 px-5 mx-auto">
+    <h1>Envía un formulario a un E-Mail desde PHP</h1>
+    <?php if (isset($_SESSION["flash"])): ?>
+      <div class="alert alert-success alert-dismissible  bg-success text-white fade show mt-3" role="alert">
+        <i class="bi bi-check-circle-fill"></i>
+        <strong>Mensaje Enviado!</strong> El mensaje a sido enviado correctamente a <strong><?= $_SESSION["flash"]["email"] ?>  
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      <?php unset($_SESSION["flash"]) ?>
+    <?php endif ?>
+    <form class="row g-3" action="send_mail.php" method="POST">
+      <div class="col-md-4">
+        <label class="form-label" for="name">Nombre</label>
+        <input type="text" class="form-control" placeholder="Su nombre" id="name" name="name" required>
+      </div>
+      <div class="col-md-4">
+        <label class="form-label" for="surename">Apellidos</label>
+        <input type="text" class="form-control" placeholder="Sus apellidos" id="surename" name="surename" required>
+      </div>
+      <div class="col-md-4">
+        <label class="form-label" for="user">Usuario</label>
+        <div class="input-group">
+          <span class="input-group-text" id="inputGroupPrepend2">@</span>
+          <input type="text" class="form-control" placeholder="Usuario habitual" id="user" name="user" aria-describedby="inputGroupPrepend2" required>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <label class="form-label" user="email">Correo Electrónico</label>
+        <input type="text" class="form-control" placeholder="Un correo que use" id="email" name="email" required>
+      </div>
+      <div class="col-md-4">
+        <label class="form-label" for="phone_number">Teléfono</label>
+        <input type="text" class="form-control" placeholder="Su número de teléfono" id="phone_number" name="phone_number" required>
+      </div>
+      <div class="col-md-4">
+        <label class="form-label"for="country">País</label>
+        <input type="text" class="form-control" placeholder="País de residencia" id="country" name="country" required>
+      </div>
+      <div>
+        <label class="form-label" for="menssage">Mensaje</label>
+        <textarea class="form-control" placeholder="Escriba aquí su mensaje" id="menssage" name="menssage" style="height: 100px"></textarea>
+      </div>
+      <div class="col-12">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="Aceptada" id="privacy_policy" name="privacy_policy" required>
+          <label class="form-check-label">
+            <a href="###">Política de Privacida</a>
+          </label>
+        </div>
+      </div>
+      <div class="col-12">
+        <button class="btn btn-primary" type="submit">Enviar Mensaje</button>
+      </div>
+    </form>
+  </div>
+</body>
+</html>
--- send_mail.php
+++ send_mail.php
@@ -1,6 +1,4 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -9,53 +7,50 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-//Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
+header("Location: index.php");
 
 $contenido = '
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-                </head>
-                <body>
-                    <p>Nombre: '.$_POST["name"].'</p>
-                    <p>E-Mail: '.$_POST["email"].'</p>
-                    <p>Telefono: '.$_POST["telefono"].'</p>
-                    <p>Mensaje: '.$_POST["mensaje"].'</p>
-                </body>
-                </html>
-            ';
-
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Nuevo Mensaje</title>
+</head>
+<body>
+    <p><strong>Nombre</strong>: '.$_POST["name"].'</p>
+    <p><strong>Apellidos</strong>: '.$_POST["surename"].'</p>
+    <p><strong>Usuario</strong>: '.$_POST["user"].'</p>
+    <p><strong>Correo Electrónico</strong>: '.$_POST["email"].'</p>
+    <p><strong>Teléfono</strong>: '.$_POST["phone_number"].'</p>
+    <p><strong>País</strong>: '.$_POST["country"].'</p>
+    <p><strong>Mensaje</strong>: '.$_POST["menssage"].'</p>
+    <p><strong>Política de Privacidad</strong>: '.$_POST["privacy_policy"].'</p>
+</body>
+</html>
+';
 
 try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.###.###';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = '###@###.###';                     //SMTP username
-    $mail->Password   = '####';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.###.###';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = '###@###.###';
+    $mail->Password   = '###';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;          
+    $mail->Port       = 465;
+                
     $mail->setFrom('###@###.###', 'alejandroalsa.es');
-    $mail->addAddress('###@###.###', 'alejandroalsa.es');     //Add a recipient
-
-    //Content
-    $mail->isHTML(true);         
-                            //Set email format to HTML
-    $mail->Subject = 'Ok';
+    $mail->addAddress($_POST["email"], 'alejandroalsa.es');
+    
+    $mail->isHTML(true);
+    $mail->Subject = 'Mensaje Recibido';
     $mail->Body    = $contenido;
     $mail->CharSet = 'UTF-8';
     $mail->send();
-    echo 'Message has been sent';
+    
+    $_SESSION["flash"] = ["email" => "{$_POST['email']}"];
 
-    header("Location: templante.html");
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "No se pudo enviar el mensaje. Error de correo: {$mail->ErrorInfo}";
 }
